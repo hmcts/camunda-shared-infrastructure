ac634df01ea59205419bc8a595e79de1c23cad3c
Tim Jacomb <tim.jacomb@hmcts.net>
2020-08-04 17:04:28 +0100

    Create MI

--- vault.tf
+++ vault.tf
@@ -8,5 +8,6 @@ module "vault" {
   resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "300e771f-856c-45cc-b899-40d78281e9c1"
   managed_identity_object_id = "${var.managed_identity_object_id}"
+  create_managed_identity = true
   common_tags             = "${var.common_tags}"
 }
