99d2a7f76580be423031950c5d9fde79c3663ebb
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-08-06 09:22:50 +0100

    remove

--- main.tf
+++ main.tf
@@ -6,14 +6,3 @@ provider "azurerm" {
   version = "=2.20.0"
   features {}
 }
-
-locals {
-  vault_name = "${var.product}-${var.env}"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.env}"
-  location = var.location
-
-  tags = var.common_tags
-}
