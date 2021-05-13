c14875ed97394cd9e1fc0bb009c8aa62e1f94fbb
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2021-05-13 10:29:06 +0100

    Add ES for optimize (#7)

---
+++ elasticsearch.tf
@@ -0,0 +1,82 @@
+provider "azurerm" {
+  alias                      = "aks-infra"
+  subscription_id            = var.aks_subscription_id
+  skip_provider_registration = true
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "mgmt"
+  subscription_id = var.mgmt_subscription_id
+  features {}
+}
+
+module "elastic" {
+  source                        = "git@github.com:hmcts/cnp-module-elk.git?ref=7.11.1"
+  product                       = var.product
+  location                      = var.location
+  env                           = var.env
+  subscription                  = var.subscription
+  common_tags                   = var.common_tags
+  dataNodesAreMasterEligible    = true
+  vmDataNodeCount               = var.vmDataNodeCount
+  vmSizeAllNodes                = var.vmSizeAllNodes
+  storageAccountType            = var.storageAccountType
+  vmDataDiskCount               = 1
+  ssh_elastic_search_public_key = data.azurerm_key_vault_secret.camunda_elastic_search_public_key.value
+  providers = {
+    azurerm           = azurerm
+    azurerm.mgmt      = azurerm.mgmt
+    azurerm.aks-infra = azurerm.aks-infra
+  }
+  logAnalyticsId      = data.azurerm_log_analytics_workspace.log_analytics.workspace_id
+  logAnalyticsKey     = data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key
+  dynatrace_instance  = var.dynatrace_instance
+  dynatrace_hostgroup = var.dynatrace_hostgroup
+  dynatrace_token     = data.azurerm_key_vault_secret.dynatrace_token.value
+  enable_logstash     = false
+  enable_kibana       = false
+  alerts_email        = data.azurerm_key_vault_secret.alerts_email.value
+}
+
+locals {
+  // Vault name
+  vaultName = "${var.product}-${var.env}"
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+data "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "hmcts-${var.subscription}"
+  resource_group_name = "oms-automation"
+}
+
+data "azurerm_key_vault_secret" "camunda_elastic_search_public_key" {
+  name         = "${var.product}-ELASTIC-SEARCH-PUB-KEY"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "dynatrace_token" {
+  name         = "dynatrace-token"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "elastic_search_url_key_setting" {
+  name         = "${var.product}-ELASTIC-SEARCH-URL"
+  value        = module.elastic.loadbalancerManual
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "elastic_search_pwd_key_setting" {
+  name         = "${var.product}-ELASTIC-SEARCH-PASSWORD"
+  value        = module.elastic.elasticsearch_admin_password
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "alerts_email" {
+  name         = "alerts-email"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "common_tags" {
-  type = "map"
+  type = map
 }
-
 variable "product" {}
 
 variable "component" {}
@@ -9,3 +8,39 @@ variable "component" {}
 variable "subscription" {}
 
 variable "env" {}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID of the Azure Active Directory"
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "This is the ID of the Application you wish to give access to the Key Vault via the access policy"
+}
+
+variable "aks_subscription_id" {}
+
+variable "mgmt_subscription_id" {}
+
+variable "vmDataNodeCount" {
+  description = "number of data nodes"
+  type        = number
+  default     = 1
+}
+
+variable "vmSizeAllNodes" {
+  description = "vm size for all the cluster nodes"
+  default     = "Standard_D2s_v3"
+}
+
+variable "storageAccountType" {
+  description = "disk storage account type"
+  default     = "Standard"
+}
+
+variable "dynatrace_instance" {}
+
+variable "dynatrace_hostgroup" {}
